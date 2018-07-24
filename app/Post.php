cf40bbb2e048ba2272fa41252271320173839f5a
kratos619 <gaurav.palaspagar@hotmail.com>
2018-07-24 13:06:16 +0530

    add soft deletes and seeders

--- app/Post.php
+++ app/Post.php
@@ -3,11 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
+    use  SoftDeletes;
     //
     protected $fillable = ['title','content','category_id','featured'];
+    protected $dates = ['deleted_at'];
     public function category(){
         return $this->belongsTo('App\category');
     }
