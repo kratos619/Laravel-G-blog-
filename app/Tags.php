eaf1d11b861e965925c0ba5b89fc172683738c7b
kratos619 <gaurav.palaspagar@hotmail.com>
2018-07-29 20:04:36 +0530

    Tag CRUD START

--- app/Tags.php
+++ app/Tags.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tags extends Model
 {
-    //
+    protected $fillable = ['tag'];
     function posts(){
         return $this-> belongsToMany('App\Post');
     }
